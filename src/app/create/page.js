b8d44982f0bcf6a57219908b5f44f72a85f1e495
simmmba <jsy9532@gmail.com>
2023-08-26 22:40:45 +0900

    Feat: Create and learn Next.js projet

---
+++ src/app/create/page.js
@@ -0,0 +1,8 @@
+export default function Create() {
+  return (
+    <div>
+      <h2>Create</h2>
+      Hello, Create!
+    </div>
+  )
+}
---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,12 @@
+export default async function Read({params}) {
+    // console.log(params);
+    const response = await fetch('http://localhost:9999/topics/'+params.id)
+    const topic = await response.json();
+    return (
+      <>
+        <h2>{topic.title}</h2>
+        {topic.body}
+      </>
+    )
+  }
+  
